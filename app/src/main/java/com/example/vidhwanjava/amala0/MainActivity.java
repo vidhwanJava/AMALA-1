ecbbfd16b158fde082731665e2b453f3c2b73454
athulkrishnan97 <athulkrishnan97@gmail.com>
2018-03-23 13:32:40 +0530

    json parsing done

--- app/src/main/java/com/example/vidhwanjava/amala0/MainActivity.java
+++ app/src/main/java/com/example/vidhwanjava/amala0/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.vidhwanjava.amala0;
 
+import android.os.AsyncTask;
 import android.os.CountDownTimer;
-import android.os.Handler;
 import android.os.SystemClock;
-import android.support.v4.widget.ListViewAutoScrollHelper;
-import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewTreeObserver;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView1;
@@ -26,8 +33,16 @@ public class MainActivity extends AppCompatActivity {
     final int heightToScroll = 2;
     final int negHeightToScroll = -2;// will be scrolled to 20 px every time. smaller values for smoother scrolling
 
-
-
+    String code;
+    String hopapatient;
+    String scanimage;
+    List<String> codeArray;
+    List<String> hopapatientArray;
+    List<String> scanimageArray;
+    String words;
+    ArrayAdapter<String> firstListAdapter;
+    ArrayAdapter<String> secondListAdapter;
+    ArrayAdapter<String> thirdListAdapter;
 
 
     @Override
@@ -40,12 +55,28 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        int listViewSize = listView1.getCount();
+
+
+
+       /* int listViewSize = listView1.getCount();
         String listv=""+listViewSize;
-        Toast.makeText(getApplicationContext(),listv,Toast.LENGTH_SHORT).show();
-       final long offset=(long)(2*totalScrollTime);
+        Toast.makeText(getApplicationContext(),listv,Toast.LENGTH_SHORT).show();*/
 
 
+        final long offset=(long)(2*totalScrollTime);
+
+        // Get the json file from the link http://122.15.159.161:4422/webservice/scanimage/all
+
+        new getJsonFile().execute();
+
+
+
+
+
+
+
+
+        //
 
 
         Runnable mynewRunnable = new Runnable() {
@@ -71,10 +102,113 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    // Function to set data from the json to the listviews. It cannot be written in onCreate, thus this function.
+public void useArrayAdapter(){
+    firstListAdapter =new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, codeArray);
+
+    listView1.setAdapter(firstListAdapter);
+
+    secondListAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, hopapatientArray);
+
+    listView2.setAdapter(secondListAdapter);
+
+    thirdListAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, scanimageArray);
+
+    listView3.setAdapter(thirdListAdapter);
+
+
+
+
+
+}
+
+
+    public class getJsonFile extends AsyncTask<Void, Void, Void>{
+
+
+
+        @Override
+        protected Void doInBackground(Void... params){
+
+            try {
+
+                Document doc =Jsoup.connect("http://122.15.159.161:4422/webservice/scanimage/all").ignoreContentType(true).get();
+                words=doc.text();
+
+            } catch (IOException e) {
+                    words=e.toString();
+                e.printStackTrace();
+
+
+            }
+
+            return null;
+        }
+
+
+        @Override
+
+        protected void onPostExecute(Void aVoid){
+            super.onPostExecute(aVoid);
+
+            showtext();
+            JsonParser();
+            useArrayAdapter();
+        }
+    }
+
+
+    public void JsonParser(){
+
+        String JSON_STRING =words;
+        codeArray = new ArrayList<String>();
+        hopapatientArray= new ArrayList<String>();
+        scanimageArray= new ArrayList<String>();
+        try {
+            JSONArray jsonResponse = new JSONArray(JSON_STRING);
+
+            for (int i = 0; i < jsonResponse.length(); i++) {
+                JSONObject actor = jsonResponse.getJSONObject(i);
+                code = actor.getString("code");
+                codeArray.add(code);
+                hopapatient=actor.getString("hopapatient");
+                hopapatientArray.add(hopapatient);
+                scanimage=actor.getString("scanimage");
+                scanimageArray.add(scanimage);
+                //Toast.makeText(this,code,Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this,hopapatient,Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            Log.d("error",e.toString());
+            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+
+    public void showtext(){
+        Toast.makeText(this,words,Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+
+
+
+
+
     public void scroller1(){
+
         Runnable myRunnable = new Runnable() {
             @Override
             public void run() {
+
 
                 listView1.post(new Runnable() {
 
